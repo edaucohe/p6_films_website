f8ead191e494b3deebb3c3667698c27754551eab
Antoine Latrille <antoine.latrille@gmail.com>
2022-04-20 13:13:21 +0200

    refactor(js): utilisation du window.onload pour le code d'orchestration et exemples avec l'api fetch

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,5 @@
 // JS Modal code
-let btn = document.getElementsByClassName('btn');
-let modal = document.getElementById('modal');
-let close = document.getElementsByClassName('close')[0];
-
-function openModal (clickingElement){
+function addOpenModalBehavior (clickingElement, modal){
   for (let element of clickingElement){
       element.addEventListener("click", function(event){
       event.preventDefault();
@@ -12,15 +8,36 @@ function openModal (clickingElement){
   }
 }
 
-function closeModal(clickingElement){
+function addClosingModalBehavior(clickingElement, modal){
   clickingElement.addEventListener("click", function(event){
     event.preventDefault();
-    if (event.target == clickingElement) {
+    if (event.target === clickingElement) {
       modal.style.display = "none";
     }
   });
 }
 
-openModal(btn);
-closeModal(close);
-closeModal(modal);
+function getMovieDetails(movieId){
+    return fetch('api-url-for-movie').then(response => response.json())
+}
+
+function listMoviesForCategory(searchParams) {
+    const params = new URLSearchParams({
+        page_size: 12,
+        category: searchParams.category
+    })
+    return fetch('....?' + params.toString()).then(response => response.json())
+}
+
+window.onload = function() {
+    let btn = document.getElementsByClassName('btn');
+    let modal = document.getElementById('modal');
+    let close = document.getElementsByClassName('close')[0];
+
+    // Add modal behavior
+    addOpenModalBehavior(btn, modal);
+    addClosingModalBehavior(close, modal);
+    addClosingModalBehavior(modal, modal);
+
+
+}
